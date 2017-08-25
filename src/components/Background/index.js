960385ed700255fe0d059f0b160282fce2550e3b
Matt Martin <08martinm@gmail.com>
2017-08-24 17:09:55 -0700

    updated splash transaction size on zoom, background copy

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import styles from './styles/styles.css';
+import styles from './styles/background.css';
 
 const Background = () => {
   return (
-    <div className={styles.div}>
+    <div className={styles.container}>
+      <h1>What is this site about?</h1>
       <div className={styles.text}>
-        <h1>What is this site about?</h1>
         <p>
-          These are my scribbling about blockchain. What it is. What it can do. And why we should care. 
-          Over time, I hope to over time host live demos and cultivate reader feedback.
-          <div className={styles.filler}></div>
+          These are my scribbling about blockchain. What it is, what it can do, and why we should care. 
+          Over time, I hope to host live demos and cultivate reader feedback.
+          <br /><br />
+          Meanwhile, if you would like to be alerted whenever I publish a new feature,
+          feel free to add your email. I promise to respect your privacy and only notify you of substantive updates.
         </p>
+        <div className={styles.center}>
+          <input type='text' placeholder='email address'/>
+          <button>Submit</button>
+        </div>
       </div>
     </div>
   );
